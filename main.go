050d3d4342fb2505dafe8d5980db867e9913f782
george <webstergd@gmail.com>
2016-08-26 13:58:14 +0200

    Moved config locations to config directory. Adjusted naming. Changed ip addresses to localhost.

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	if confPath == "" {
 		confPath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
-		confPath += "/config.json"
+		confPath += "/config/gateway.conf"
 	}
 	if master {
 		mastergateway.Start(confPath)
